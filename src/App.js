325f1998c7d695a65f12aeacf65e635d9b3c6fef
LuoHao-RenRen <hluo5206@gmail.com>
2020-05-19 15:41:16 +0800

    feat: 导航栏

--- src/App.js
+++ src/App.js
@@ -6,60 +6,93 @@ import CodeSegment from '@/views/code-segment';
 import Compression from '@/views/file-compression';
 import ModuleMerge from '@/views/module-merge';
 import Transcoder from '@/views/transcoder';
+import '@/assets/scss/app.scss';
 
-function App() {
-	return (
-		<Router>
-			<div className='root'>
-				<div className='navigator'>
-					<ul>
-						<li>
-							<Link to="/transcoder">代码转换</Link>
-						</li>
-						<li>
-							<Link to="/compression">文件压缩</Link>
-						</li>
-						<li>
-							<Link to="/code-segment">代码分割</Link>
-						</li>
-						<li>
-							<Link to="/module-merge">模块合并</Link>
-						</li>
-						<li>
-							<Link to="/auto-refresh">自动刷新</Link>
-						</li>
-					</ul>
-				</div>
-				<div className='content'>
-					<Switch>
-						<Route path="/home">
-							<Home/>
-						</Route>
-						<Route path="/auto-refresh">
-							<AutoRefresh/>
-						</Route>
-						<Route path="/code-segment">
-							<CodeSegment/>
-						</Route>
-						<Route path="/compression">
-							<Compression/>
-						</Route>
-						<Route path="/module-merge">
-							<ModuleMerge/>
-						</Route>
-						<Route path="/transcoder">
-							<Transcoder/>
-						</Route>
-						<Redirect to="/home"/>
-					</Switch>
-					
-				</div>
-				<div className='foot'>
+const navs = [
+	{
+		label: '代码转换',
+		value: '/transcoder'
+	},
+	{
+		label: '文件压缩',
+		value: '/compression'
+	},
+	{
+		label: '代码分割',
+		value: '/code-segment'
+	},
+	{
+		label: '模块合并',
+		value: '/module-merge'
+	},
+	{
+		label: '自动刷新',
+		value: '/auto-refresh'
+	}
+];
 
+class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			route: '/home'
+		}
+		this.changeRoute = this.changeRoute.bind(this);
+	}
+
+	render() {
+		return (
+			<Router>
+				<div className='root'>
+					<div className='navigator'>
+						<ul>
+							{
+								navs.map((elem, index) => (
+									<li key={index} 
+										onClick={() => {this.changeRoute(elem.value)}}
+										className={this.state.route === elem.value ? 'active': null}>
+										<Link to={elem.value}>{elem.label}</Link>
+									</li>
+								))
+							}
+						</ul>
+					</div>
+					<div className='content'>
+						<Switch>
+							<Route path="/home">
+								<Home />
+							</Route>
+							<Route path="/auto-refresh">
+								<AutoRefresh />
+							</Route>
+							<Route path="/code-segment">
+								<CodeSegment />
+							</Route>
+							<Route path="/compression">
+								<Compression />
+							</Route>
+							<Route path="/module-merge">
+								<ModuleMerge />
+							</Route>
+							<Route path="/transcoder">
+								<Transcoder />
+							</Route>
+							<Redirect to="/home" />
+						</Switch>
+
+					</div>
+					<div className='foot'></div>
 				</div>
-			</div>
-		</Router>
-	)
+			</Router>
+		)
+	}
+
+	changeRoute(value) {
+		this.setState({
+			route: value
+		});
+	}
+
 }
 
 export default App;
